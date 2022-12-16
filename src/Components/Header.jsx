ddebdc027b67675fad46edd8a2a2ac1654d8732b
bablu <bablukumar@techis.io>
2022-12-16 15:08:15 +0530

    Final_Push

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../Styles/techis-logo.png'
+
+function Header() {
+  return (
+    <div>
+        <header>
+            <h3>Python IDE</h3>
+            <img src={logo} alt="" />
+        </header>
+    </div>
+  )
+}
+
+export default Header
